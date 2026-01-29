1817d603d01168fcc2f9fc1eefe0971a0a619f36
Сиразов Айрат <sirazov@infomatika.ru>
2026-01-29 17:52:49 +0300

    feat: add base for custom errors

--- week_8/custom_errors/go.mod
+++ week_8/custom_errors/go.mod
@@ -3,6 +3,17 @@ module github.com/AiratS/micro_as_bigtech_course/week_8/custom_errors
 go 1.25.3
 
 require (
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.6.0 // indirect
-	google.golang.org/protobuf v1.36.11 // indirect
+	github.com/AiratS/micro_as_bigtech_course/platform_common v0.0.0-20260129142715-313f4a534e32
+	github.com/golang/protobuf v1.5.4
+	google.golang.org/grpc v1.70.0
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.34.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 )
--- week_8/custom_errors/internal/interceptor/error.go
+++ week_8/custom_errors/internal/interceptor/error.go
@@ -1,1 +1,76 @@
 package interceptor
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+
+	"github.com/AiratS/micro_as_bigtech_course/platform_common/pkg/sys"
+	"github.com/AiratS/micro_as_bigtech_course/platform_common/pkg/sys/codes"
+	grpcCodes "google.golang.org/grpc/codes"
+)
+
+type GRPCStatusInterface interface {
+	GRPCStatus() *status.Status
+}
+
+func ErrorCodesInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	res, err := handler(ctx, req)
+	if err == nil {
+		return res, err
+	}
+
+	switch {
+	case sys.IsCommonError(err):
+		commErr := sys.GetCommonError(err)
+		code := toGRPCCode(commErr.Code())
+
+		err = status.Error(code, commErr.Error())
+	}
+
+	return res, err
+}
+
+func toGRPCCode(code codes.Code) grpcCodes.Code {
+	var res grpcCodes.Code
+
+	switch code {
+	case codes.OK:
+		res = grpcCodes.OK
+	case codes.Canceled:
+		res = grpcCodes.Canceled
+	case codes.InvalidArgument:
+		res = grpcCodes.InvalidArgument
+	case codes.DeadlineExceeded:
+		res = grpcCodes.DeadlineExceeded
+	case codes.NotFound:
+		res = grpcCodes.NotFound
+	case codes.AlreadyExists:
+		res = grpcCodes.AlreadyExists
+	case codes.PermissionDenied:
+		res = grpcCodes.PermissionDenied
+	case codes.ResourceExhausted:
+		res = grpcCodes.ResourceExhausted
+	case codes.FailedPrecondition:
+		res = grpcCodes.FailedPrecondition
+	case codes.Aborted:
+		res = grpcCodes.Aborted
+	case codes.OutOfRange:
+		res = grpcCodes.OutOfRange
+	case codes.Unimplemented:
+		res = grpcCodes.Unimplemented
+	case codes.Internal:
+		res = grpcCodes.Internal
+	case codes.Unavailable:
+		res = grpcCodes.Unavailable
+	case codes.DataLoss:
+		res = grpcCodes.DataLoss
+	case codes.Unauthenticated:
+		res = grpcCodes.Unauthenticated
+	default:
+		res = grpcCodes.Unknown
+	}
+
+	return res
+}
